6dc865a095ce74354fc237c04bfa4655b86cb99e
willy95 <wilvardo@gmail.com>
2017-05-23 16:37:13 -0500

    conexion al socket y pre-funcionalidad con pre-diseño

--- app/Ws/Controllers/BusTicketController.js
+++ app/Ws/Controllers/BusTicketController.js
@@ -5,10 +5,15 @@ class BusTicketController {
     constructor(socket, request) {
         this.socket = socket
         this.request = request
+        console.log('socket connected', socket.id);
     }
 
     onMessage(message) {
-        
+        this.socket.toEveryone().emit('message', message);
+    }
+
+    disconnected(socket){
+        console.log('socket disconnected', socket.id);
     }
 
 }
