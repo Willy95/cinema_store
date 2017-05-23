ead6001dbb189716315da025d6f42c3914918135
willy95 <wilvardo@gmail.com>
2017-05-23 14:12:27 -0500

    first

---
+++ app/Ws/Controllers/BusTicketController.js
@@ -0,0 +1,16 @@
+'use strict'
+
+class BusTicketController {
+
+    constructor(socket, request) {
+        this.socket = socket
+        this.request = request
+    }
+
+    onMessage(message) {
+        
+    }
+
+}
+
+module.exports = BusTicketController
