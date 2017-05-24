ccc45ccc5e713d055b042051b2ea97d1a6b71dbb
canales95 <fernando.canales@curiosity.com.mx>
2017-05-23 21:49:18 -0500

    instale moongose, decidi que mejor con mongo e hice sus configuraciones.

---
+++ app/Model/Cliente.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const Mongo = use('Mongoose')
+
+let ClienteSchema = Mongo.Schema({
+    nombre:{type:String, required: true},
+    apellido:{type:String, required: true},
+    destino:{type:String, required: true}
+}),{collection:'clientes'}
+
+module.exports = Mongo.model('ClienteSchema',ClienteSchema)
