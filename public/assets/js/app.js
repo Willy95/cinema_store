86aa5176ec7b2120a828e35037e1b6c9a5559198
willy95 <wilvardo@gmail.com>
2017-05-23 22:00:16 -0500

    mis avances

---
+++ public/assets/js/app.js
@@ -0,0 +1,49 @@
+$(function(){
+
+    const io = ws('');
+    const client = io.channel('assign_ticket').connect(console.log);
+
+    var sites_limit = 1;
+    var siteMsg = { site: null, type: null };
+
+    $(".site").click(function(event) {
+        if ($(this).hasClass('site-pre-assigned-sess') ||
+        $(this).hasClass('site-pre-assigned') ||
+        $(this).hasClass('site-assigned')) {
+            siteMsg.site = $(this).data('numsite');
+            siteMsg.type = "unassigned";
+            client.emit('message', siteMsg);
+            $(this).removeClass('site-pre-assigned-sess');
+        }
+        else {
+            if ( $(".site-pre-assigned-sess").length < sites_limit ){
+                siteMsg.site = $(this).data('numsite');
+                siteMsg.type = "pre-assigned";
+                client.emit('message', siteMsg);
+                $(this).addClass('site-pre-assigned-sess');
+            }
+            else{
+                alert("Has alcanzado el límite de asientos seleccionados");
+            }
+        }
+    });
+
+    function selectSite(message){
+        var site = $("body").find('#' + message.site);
+        if ( !site.hasClass('site-pre-assigned-sess' )){
+            switch ( message.type ) {
+                case "pre-assigned":
+                    site.removeClass('site');
+                    site.addClass('site-pre-assigned');
+                break;
+                case "unassigned":
+                    site.removeClass('site-pre-assigned');
+                    site.addClass('site');
+                break;
+            }
+        }
+    }
+
+    client.on('message', selectSite);
+
+});
