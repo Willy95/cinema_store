edcb3a1d9ac4789e0d27d7233cc70d15efa51b1f
canales95 <fernando.canales@curiosity.com.mx>
2017-06-27 10:39:39 -0500

    cambios de crud para peliculas.

--- public/scripts/movies/src/moviegesController.js
+++ public/scripts/movies/src/moviegesController.js
@@ -3,7 +3,39 @@ class moviegesController {
 
     }
 
-    static saveMovie(success,error){
-        
+    static saveMovie(data,success,error){
+        $.ajax({
+            url: 'register-movie',
+            type: 'post',
+            data: { data }
+        }).done(function(r){
+            success(r);
+        }).fail(function(e){
+            error(e);
+        })
+    }
+
+    static updateMovie(data,success,error){
+        $.ajax({
+            url: 'update-movie',
+            type: 'post',
+            data: { data }
+        }).done(function(r){
+            success(r);
+        }).fail(function(e){
+            error(e);
+        })
+    }
+
+    static deleteMovie(data,success,error){
+        $.ajax({
+            url: 'delete-movie',
+            type: 'post',
+            data: { data }
+        }).done(function(r){
+            success(r);
+        }).fail(function(e){
+            error(e);
+        })
     }
 }
--- public/scripts/movies/src/moviegesDispatcher.js
+++ public/scripts/movies/src/moviegesDispatcher.js
@@ -1,3 +1,41 @@
 $(function(){
-    
+
+    function getDataForm() {
+        let data = {
+            title:   $("#title").val(),
+            poster:  $("#poster").val()
+            year:    $("#year").val()
+            runtime: $("#runtime").val()
+            trailer: $("#trailer").val()
+            synopsis:$("#synopsis").val()
+        }
+        return data;
+    } // mandar los datos del form como un objecto
+
+    function successAlert(res) {
+        switch (res.status) {
+            case 'c200':
+                toastr.success(res.msj);
+                break;
+            case 'c403':
+                toastr.warning(res.msj)
+                break;
+            default:
+                toastr.error('Error desconocido, intentalo nuevamente más tarde');
+
+        }
+
+    }
+
+    function errorAlert(err) {
+        toastr.error(error)
+        console.log(err);
+    }
+
+
+    $("#save-movie").click(function(event){
+        let movie = getDataForm();
+        
+    })
+
 })
